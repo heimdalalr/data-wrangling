900ad0aed8d0c411d55203cd701310ac8d88de05
Rae Carter <92331343+heimdalalr@users.noreply.github.com>
2021-12-28 11:29:09 -0500

    working on transforming data

--- data-wrangling.py
+++ data-wrangling.py
@@ -4,14 +4,14 @@ from pandas.io.formats.format import return_docstring
 
 df = pd.read_csv('Film_Locations.csv')
 
-print(df.shape)
-# print(df.info())
+# print(df.shape)
+print(df.info())
 # print(df.head())
 # print(df.tail())
 
 # print(df.describe())
 # print(df.mean())
-print(df['Release Year'].max())
+# print(df['Release Year'].max())
 
 
 # Step 2: Select, Filter, Sort
@@ -20,23 +20,29 @@ print(df['Release Year'].max())
 # write a function that produces a subset of data that inclues a numeric value that is higher than average
 def find_above_avg_year():
     avg = df['Release Year'].mean()
-    avg = avg.round()
     return df[ df['Release Year'] > avg]
 
-print(find_above_avg_year())
+# print(find_above_avg_year())
 
 # write a function that sorts the rows alpabetically based on a textual column.
 def sort_by_locale():
-    columns = df[['Locations', 'Fun Facts', 'Productions Company', 'Director', 'Actor 1']]
+    columns = df[['Locations', 'Fun Facts', 'Production Company', 'Director', 'Actor 1']]
     return columns.sort_values(by='Locations', ascending=True)
 
-print(sort_by_locale())
+# print(sort_by_locale())
 
 
 # Step 3: Clean Data
 # Identify whether your dataset has any missing or null value using pandas. If there are null values in a numeric column, determine whether you should remove the data, or if there is a reasonable replacement for null, such as replacing it with a 0 value. Additionally, identify if there are any low/high outliers in your dataset using pandas.
 
+#detect missing values
+# print(df.isnull())
+#will show true if any missing values appear in the column and false if all items are filled in with a value
 print(df.isnull())
+#detect if any column is completely empty, would come up as True if empty
+print(df.isnull().all())
+#detect if there is any duplicate values in locations
+print(df.['Locations'].duplicated())
 
 # Step 4: Transform Data
 # Conduct 1-2 data tranformations on your dataset, such as indexing your data with a useful numeric value, or combining multiple columns into a new column.
